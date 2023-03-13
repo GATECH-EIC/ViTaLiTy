d0a7947afe70ba809d8b956944a6f57cb7efbca9
ShangWu <44118708+Windsao@users.noreply.github.com>
2023-03-13 14:27:39 -0500

    fix a bug

--- src/vision_transformer.py
+++ src/vision_transformer.py
@@ -149,7 +149,7 @@ class Block(nn.Module):
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
-        #self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
     def forward(self, x):
         x, attn_list = x
